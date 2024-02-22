664d22069f8a0c2482b4f839c83fb04885912aa7
SaujanBindukar <34705432+SaujanBindukar@users.noreply.github.com>
2024-02-22 20:10:01 +0545

    removed unused codes

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,6 +1,6 @@
 import 'package:apexive_test/core/router/routes.dart';
-import 'package:apexive_test/feature/album/presentation/page/album_page.dart';
 import 'package:apexive_test/feature/timer/presentation/page/create_timesheet_screen.dart';
+import 'package:apexive_test/feature/timer/presentation/page/timer_screen.dart';
 import 'package:apexive_test/feature/timer/presentation/page/timesheet_details_screen.dart';
 import 'package:go_router/go_router.dart';
 
@@ -8,7 +8,7 @@ final GoRouter router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) => const AlbumPage(),
+      builder: (context, state) => const TimerScreen(),
     ),
     GoRoute(
       path: Routes.createTimeSheetScreen,
