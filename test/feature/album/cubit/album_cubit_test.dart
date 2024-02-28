6d7906ec8904545270899152618ba0e5df79182f
SaujanBindukar <34705432+SaujanBindukar@users.noreply.github.com>
2024-02-28 21:46:54 +0545

    used mock repo for bloc test

--- test/feature/album/cubit/album_cubit_test.dart
+++ test/feature/album/cubit/album_cubit_test.dart
@@ -1,21 +1,28 @@
 import 'package:apexive_test/feature/album/cubit/album_cubit.dart';
+import 'package:apexive_test/feature/album/infrastructure/model/album_response.dart';
+import 'package:apexive_test/feature/album/infrastructure/respository/album_repository.dart';
 import 'package:bloc_test/bloc_test.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:mocktail/mocktail.dart';
 
-import '../infrastructure/respository/mock_album_repository.dart';
+const mockAlbumData = [
+  AlbumData(id: 1, userId: 1, title: 'title 1'),
+  AlbumData(id: 2, userId: 2, title: 'title 2'),
+  AlbumData(id: 3, userId: 3, title: 'title 3'),
+];
+
+class MockAlbumRepository extends Mock implements AlbumRepository {}
 
 void main() {
-  late AlbumCubit albumCubit;
-  MockAlbumRepository mockAlbumRepository;
-
-  setUp(() {
-    mockAlbumRepository = MockAlbumRepository();
-    albumCubit = AlbumCubit(albumRepository: mockAlbumRepository);
-  });
-
   blocTest<AlbumCubit, AlbumState>(
     'emits album loaded when getAlbumList is called',
-    build: () => albumCubit,
+    build: () {
+      final mockAlbumRepository = MockAlbumRepository();
+      when(() => mockAlbumRepository.getAlbumList())
+          .thenAnswer((_) async => right(mockAlbumData));
+      final albumCubit = AlbumCubit(albumRepository: mockAlbumRepository);
+      return albumCubit;
+    },
     act: (bloc) => bloc.getAlbumList(),
     expect: () => <AlbumState>[
       AlbumLoading(),
@@ -23,14 +30,16 @@ void main() {
     ],
   );
 
-  //bloc test for failure
-  // blocTest<AlbumCubit, AlbumState>(
-  //   'emits album error when getAlbumList is called',
-  //   build: () => albumCubit,
-  //   act: (bloc) => bloc.getAlbumList(),
-  //   expect: () => <AlbumState>[
-  //     AlbumLoading(),
-  //     const AlbumError(message: 'Something went wrong')
-  //   ],
-  // );
+  blocTest<AlbumCubit, AlbumState>(
+    'emits empty album state when getAlbumList is called',
+    build: () {
+      final mockAlbumRepository = MockAlbumRepository();
+      when(() => mockAlbumRepository.getAlbumList())
+          .thenAnswer((_) async => right([]));
+      final albumCubit = AlbumCubit(albumRepository: mockAlbumRepository);
+      return albumCubit;
+    },
+    act: (bloc) => bloc.getAlbumList(),
+    expect: () => <AlbumState>[AlbumLoading(), AlbumEmpty()],
+  );
 }
